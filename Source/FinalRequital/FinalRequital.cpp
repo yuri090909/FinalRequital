24386a243aa22a8080ddb08765aaa92d10d27146
gdevhun <wjh9330@naver.com>
2025-04-16 17:16:51 +0900

    Generate UrealProject

---
+++ Source/FinalRequital/FinalRequital.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FinalRequital.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FinalRequital, "FinalRequital" );
+ 
